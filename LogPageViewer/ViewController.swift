1bfa0b8ec773c5c6ffbb34e16c7eefcaac79063a
Tyler Hays <Tyler.Hays@hcss.com>
2019-04-25 18:14:43 -0500

    Initial Commit

---
+++ LogPageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogPageViewer
+//
+//  Created by Tyler Hays on 4/25/19.
+//  Copyright © 2019 Tyler Hays. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
